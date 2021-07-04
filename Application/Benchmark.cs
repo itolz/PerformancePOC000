049a590e0627b1a7bb142a7bdc0a11758d35e921
italovinicios <italovinicios@gmail.com>
2021-07-04 19:48:19 -0300

    NotMaterializedQueryTest call

---
+++ Application/Benchmark.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application
+{
+    public class BenchmarkClass
+    {
+        public void NotMaterializedQueryTest(Func<string, string> makeASound, string sound)
+        {
+            var elements = Enumerable.Range(0, 50000000);
+
+
+            var filteredElements = elements.Where(_ => _ % 100000 == 0);
+            
+
+            foreach (var element in filteredElements)
+            {
+                 makeASound(sound);
+            }
+
+            string longString = string.Empty;
+        
+            foreach (var element in filteredElements)
+            {
+            
+                longString+= element.ToString();
+            }
+
+            List<int> gtfifty = new List<int>();
+
+            foreach(var element in filteredElements)
+            {
+                if (element>50)
+                {
+                    gtfifty.Add(element);
+                }
+            }
+
+        }
+    }
+}
---
+++ Application/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Starting...");
+
+            Func<string, string> makeASound = sound => { return $"the sound goes for {sound}";};
+
+            BenchmarkClass bmark = new BenchmarkClass();
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            bmark.NotMaterializedQueryTest(makeASound, "TestingSound");
+
+            sw.Stop();
+
+            var timeSpan = sw.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
+            timeSpan.Milliseconds / 10);
+
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
