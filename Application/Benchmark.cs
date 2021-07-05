9d22b758a12d678f579fe843a661e2088255b0b0
italovinicios <italovinicios@gmail.com>
2021-07-05 10:14:36 -0300

    Materialized code implemented

--- Application/Benchmark.cs
+++ Application/Benchmark.cs
@@ -6,32 +6,59 @@ namespace Application
 {
     public class BenchmarkClass
     {
-        public void NotMaterializedQueryTest(Func<string, string> makeASound, string sound)
+        public void NotMaterializedQueryTest(IEnumerable<int> elements, Func<string, string> makeASound, string sound)
         {
-            var elements = Enumerable.Range(0, 50000000);
-
 
             var filteredElements = elements.Where(_ => _ % 100000 == 0);
-            
+
 
             foreach (var element in filteredElements)
             {
-                 makeASound(sound);
+                makeASound(sound);
             }
 
             string longString = string.Empty;
-        
+
             foreach (var element in filteredElements)
             {
-            
-                longString+= element.ToString();
+
+                longString += element.ToString();
             }
 
             List<int> gtfifty = new List<int>();
 
-            foreach(var element in filteredElements)
+            foreach (var element in filteredElements)
             {
-                if (element>50)
+                if (element > 50)
+                {
+                    gtfifty.Add(element);
+                }
+            }
+
+        }
+
+        public void MaterializedQueryTest(IEnumerable<int> elements, Func<string, string> makeASound, string sound)
+        {
+
+            var filteredElements = elements.Where(_ => _ % 100000 == 0).ToList();
+
+            foreach (var element in filteredElements)
+            {
+                makeASound(sound);
+            }
+
+            string longString = string.Empty;
+
+            foreach (var element in filteredElements)
+            {
+                longString += element.ToString();
+            }
+
+            List<int> gtfifty = new List<int>();
+
+            foreach (var element in filteredElements)
+            {
+                if (element > 50)
                 {
                     gtfifty.Add(element);
                 }
--- Application/Program.cs
+++ Application/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Application
 {
@@ -12,22 +13,40 @@ namespace Application
 
             Func<string, string> makeASound = sound => { return $"the sound goes for {sound}";};
 
+            var elements = Enumerable.Range(0, 50000000);
+
             BenchmarkClass bmark = new BenchmarkClass();
             var sw = new Stopwatch();
 
             sw.Start();
-
-            bmark.NotMaterializedQueryTest(makeASound, "TestingSound");
+ 
+            bmark.NotMaterializedQueryTest(elements, makeASound, "TestingSound");
 
             sw.Stop();
 
-            var timeSpan = sw.Elapsed;
+            var timeSpanNotMaterialized = sw.Elapsed;
 
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-            timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
-            timeSpan.Milliseconds / 10);
+            string elapsedTimeNotMaterialized = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            timeSpanNotMaterialized.Hours, timeSpanNotMaterialized.Minutes, timeSpanNotMaterialized.Seconds,
+            timeSpanNotMaterialized.Milliseconds / 10);
 
-            Console.WriteLine("RunTime " + elapsedTime);
+            Console.WriteLine("RunTime for NotMaterializedQueryTest " + elapsedTimeNotMaterialized);
+
+            sw = new Stopwatch();
+            sw.Start();
+
+            bmark.MaterializedQueryTest(elements, makeASound, "TestingSound");
+
+            sw.Stop();
+
+            var timeSpanMaterialized = sw.Elapsed;
+
+            string elapsedTimetimeSpanMaterialized = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            timeSpanMaterialized.Hours, timeSpanMaterialized.Minutes, timeSpanMaterialized.Seconds,
+            timeSpanMaterialized.Milliseconds / 10);
+
+            Console.WriteLine("RunTime for MaterializedQueryTest " + elapsedTimetimeSpanMaterialized);
+
         }
     }
 }
